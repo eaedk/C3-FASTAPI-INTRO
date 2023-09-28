3c509f7627bbb897a2e2ac85865eea6247b1cafc
Emmanuel KOUPOH <emmanuelkoupoh@gmail.com>
2023-09-28 17:38:40 +0000

    example of inference endpoint

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from typing import Union
-
+import pandas as pd
+import numpy as np
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -35,3 +36,44 @@ def calculator(val_01: float, val_02: float, operator):
     return {"value_A": val_01, "value_b": val_02,
             "string": f"{val_01} {op} {val_02} = {result}" ,  
             "result":result}
+
+# Predict endpoint
+@app.get("/inference")
+def predict(age, gender, height, ):
+    """
+    """
+
+    # Prepare the features and structure them like in the notebook
+    df = pd.DataFrame({
+        "age": [age],
+        "gender": [gender],
+        "height of father": [height], 
+    })
+
+    print(f"\n\n[Info] The initial and raw df: \n{df.to_markdown()}\n\n")
+
+    # Features preprocessing and creation
+
+    # df_scaled = scaler.transform(df[numerical_features])
+    # df_encoded = encoder.transform(df[categorical_features])
+    # df_cleaned = pd.concat([df_scaled ,df_encoded], axis=1)
+
+    # Prediction
+    # raw_prediction = model.predict(df_cleaned)
+
+    # Format the prediction to send the API's Response
+    output = {
+        "features" : {
+                    "age": age,
+                    "gender": gender,
+                    "height of father": height, 
+                    },
+        "df": df, # Pandas is now automatically converted, no more error 
+        # "array": np.array([1, 2, 3]), # Numpy arrays raise an error
+        "array": np.array([1, 2, 3]).tolist(),
+    }
+    return output
+
+if __name__ == "__main__":
+    pass
+    # uvicorn.
